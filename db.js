0e4039af4f5474bfb2f0e3e0ae2ca721ad020057
yoojh9 <yoojh9@gmail.com>
2018-12-14 16:09:47 +0900

    add home controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324393,
+        title: "awesome Video",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        creator: {
+            id: 123123,
+            name: "Jeonghyun",
+            email: "yoojh9@gmail.com"
+        }
+    },
+    {
+        id: 324394,
+        title: "super Video",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        creator: {
+            id: 123123,
+            name: "Jeonghyun",
+            email: "yoojh9@gmail.com"
+        }
+    },
+    {
+        id: 55555,
+        title: "nice Video",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        creator: {
+            id: 123123,
+            name: "Jeonghyun",
+            email: "yoojh9@gmail.com"
+        }
+    },
+    {
+        id: 11111,
+        title: "perfect Video",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        creator: {
+            id: 123123,
+            name: "Jeonghyun",
+            email: "yoojh9@gmail.com"
+        }
+    }
+]
