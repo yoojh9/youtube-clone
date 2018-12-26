d6a28b46911dd5c8835709eb1683dd92b8828d75
Jeonghyun <yoojh9@gmail.com>
2018-12-26 16:15:17 +0900

    Editing a Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -4,18 +4,27 @@ import {
     videos,  
     videoDetail, 
     deleteVideo, 
-    editVideo, 
     getUpload,
-    postUpload
+    postUpload,
+    getEditVideo,
+    postEditVideo
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);   // uploadVideo middleware 추가
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo)
 
 export default videoRouter;
