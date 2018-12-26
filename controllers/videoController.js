0b4a45c3c84ff5345e5546864f9038d374bb969a
Jeonghyun <yoojh9@gmail.com>
2018-12-26 16:54:40 +0900

    Delete Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -48,7 +48,7 @@ export const videoDetail = async(req, res) => {
     
     try {
         const video = await Video.findById(id);
-        res.render("videoDetail", { pageTitle: "Video Detail", video });
+        res.render("videoDetail", { pageTitle: video.title, video });
     } catch(error) {
         console.error(error)
         res.redirect(routes.home);
@@ -74,7 +74,7 @@ export const postEditVideo = async(req, res) => {
     } = req
     
     try {
-        await Video.findOneAndUpdate({ id }, { title, description });
+        await Video.findOneAndUpdate({ _id : id }, { title, description });
         res.redirect(routes.videoDetail(id));
     } catch(error) {
         console.error(error);
@@ -82,5 +82,16 @@ export const postEditVideo = async(req, res) => {
     }
 }
 
-export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async(req, res) => {
+    const {
+        params: {id}
+    } = req;
+
+    try {
+        await Video.findByIdAndRemove( { _id : id} )
+    } catch(error){
+        console.error(error);
+    }
+    res.redirect(routes.home);
+} 
 
