bed13bcf27dfbc4510863f298c5eb88e6021c057
Jeonghyun <yoojh9@gmail.com>
2018-12-31 16:02:15 +0900

    footer and login/join

--- README.md
+++ README.md
@@ -8,6 +8,11 @@
 - 더 명확하고 보기 좋은 코딩을 가능하게 한다.
 - [header.scss참고](https://github.com/yoojh9/youtube-clone/blob/styling/assets/scss/partials/header.scss)
 
+## 2. nodemon ignore 옵션 추가
+
+- scss 변경 사항이 있을 때마다 서버가 재시작되므로 package.json 내 scripts에 dev:server 부분에 ignore 옵션을 추가한다 "--ignore ['assets/*.scss', 'static/styles.css']"
+- 노마드코더는 "--ignore 'scss'"로 작성했는데 styles.css가 변경되도 서버가 재시작하는 것 같아 styles.css도 ignore 옵션으로 추가했다.
+
 ---
 
 ### 참고
--- middlewares.js
+++ middlewares.js
@@ -1,16 +1,16 @@
 import routes from "./routes";
 import multer from "multer";
 
-const multerVideo = multer({dest: "uploads/videos/"})
+const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localMiddleware = (req, res, next) => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    };
-    next();
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1
+  };
+  next();
 };
 
-export const uploadVideo =  multerVideo.single('videoFile');  // one file
+export const uploadVideo = multerVideo.single("videoFile"); // one file
